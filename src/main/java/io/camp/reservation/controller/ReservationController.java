0aad3b42bbb54f9be5e72c305cc1f0b5b97e75f3
HongSP99 <tmdvy0805@naver.com>
2024-07-26 21:31:40 +0900

    feat : 예약 기능 구현

---
+++ src/main/java/io/camp/reservation/controller/ReservationController.java
@@ -0,0 +1,31 @@
+package io.camp.reservation.controller;
+
+import io.camp.reservation.mapper.ReservationMapper;
+import io.camp.reservation.model.Reservation;
+import io.camp.reservation.model.dto.ReservationPostDto;
+import io.camp.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reservation")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<?> createReservation(@RequestBody ReservationPostDto postDto){
+        //동시 예약 시도 체크
+        try{
+            Reservation reservation = reservationService.createReservation(postDto);
+            return new ResponseEntity<>(reservation, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>("동시 예약이 발생했습니다. 잠시 후 다시 시도해주세요.", HttpStatus.CONFLICT);
+        }
+    }
+}
