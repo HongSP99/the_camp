9bf999cf912a613f52b773cd2499ba1f838d2b83
HongSP99 <tmdvy0805@naver.com>
2024-07-29 17:06:22 +0900

    refactor : 예약 dto 데이터 수정

--- src/main/java/io/camp/reservation/model/dto/ReservationPostDto.java
+++ src/main/java/io/camp/reservation/model/dto/ReservationPostDto.java
@@ -2,6 +2,7 @@ package io.camp.reservation.model.dto;
 
 import io.camp.user.model.User;
 import java.time.LocalDateTime;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
