8fb76e68bd0a8cdaadcddcf8eafd26b07122bdf0
minh <rnrnck78@naver.com>
2024-08-02 14:02:20 +0900

    init : querydsl 설정 수정

---
+++ src/main/generated/io/camp/reservation/mapper/ReservationMapperImpl.java
@@ -0,0 +1,31 @@
+package io.camp.reservation.mapper;
+
+import io.camp.reservation.model.Reservation;
+import io.camp.reservation.model.dto.ReservationPostDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-02T13:53:50+0900",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.8.jar, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class ReservationMapperImpl implements ReservationMapper {
+
+    @Override
+    public Reservation reservationPostDtoToReservation(ReservationPostDto reservationPostDto) {
+        if ( reservationPostDto == null ) {
+            return null;
+        }
+
+        Reservation reservation = new Reservation();
+
+        reservation.setReserveStartDate( reservationPostDto.getReserveStartDate() );
+        reservation.setReserveEndDate( reservationPostDto.getReserveEndDate() );
+        reservation.setAdults( reservationPostDto.getAdults() );
+        reservation.setChildren( reservationPostDto.getChildren() );
+
+        return reservation;
+    }
+}
