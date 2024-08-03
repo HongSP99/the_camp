316b59a1c36f0b7d25fde043ce0599a19caec49b
HongSP99 <tmdvy0805@naver.com>
2024-08-03 15:21:43 +0900

    feat : 예외 처리 코드 추가

--- src/main/java/io/camp/exception/ExceptionCode.java
+++ src/main/java/io/camp/exception/ExceptionCode.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 public enum ExceptionCode {
     // reservation
-    RESERVATION_NOT_FOUND(404, "예약을 찾을 수 없습니다.");
+    RESERVATION_NOT_FOUND(404, "예약을 찾을 수 없습니다."),
+    RESERVATION_CANNOT_BE_CANCELLED(400, "하루 전에는 예약을 할 수 없습니다.");
 
     @Getter
     private int status;
