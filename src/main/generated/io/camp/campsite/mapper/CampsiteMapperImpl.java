e18ea770fb9a8de885d804d8aedbb1d2aac92356
naaaav <naaaav@naver.com>
2024-08-07 15:10:02 +0900

    feat: 이미지 업로드

--- src/main/generated/io/camp/campsite/mapper/CampsiteMapperImpl.java
+++ src/main/generated/io/camp/campsite/mapper/CampsiteMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-03T18:22:30+0900",
-    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.8.jar, environment: Java 17.0.8 (Oracle Corporation)"
+    date = "2024-08-07T14:12:28+0900",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.8.jar, environment: Java 17.0.10 (Oracle Corporation)"
 )
 @Component
 public class CampsiteMapperImpl implements CampsiteMapper {
