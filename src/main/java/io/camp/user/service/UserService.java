2eb96eb9a530e014335edda416be46c74a36e62e
minh <rnrnck78@naver.com>
2024-08-13 16:29:44 +0900

    init : 더미유저 추가

--- src/main/java/io/camp/user/service/UserService.java
+++ src/main/java/io/camp/user/service/UserService.java
@@ -53,62 +53,62 @@ public class UserService {
         System.out.println("user.gender : " + user.getGender());
     }
 
-//    @PostConstruct
-//    public void userInit() {
-//        if (userRepository.findByEmail("admin") != null) {
-//            return;
-//        }
-//
-//        User user = new User();
-//        user.setEmail("admin");
-//        user.setPassword(passwordEncoder.encode("1234"));
-//        user.setRole(UserRole.ADMIN);
-//        user.setName("관리자");
-//        user.setBirthday("0000-00-00");
-//        user.setPhoneNumber("000-1234-5678");
-//        user.setGender("성별");
-//        userRepository.save(user);
-//
-//        user = new User();
-//        user.setEmail("user01");
-//        user.setPassword(passwordEncoder.encode("1234"));
-//        user.setRole(UserRole.USER);
-//        user.setName("홍길동");
-//        user.setBirthday("2000-01-01");
-//        user.setPhoneNumber("000-1111-1111");
-//        user.setGender("남성");
-//        userRepository.save(user);
-//
-//        user = new User();
-//        user.setEmail("user02");
-//        user.setPassword(passwordEncoder.encode("1234"));
-//        user.setRole(UserRole.USER);
-//        user.setName("블루이");
-//        user.setBirthday("2010-01-01");
-//        user.setPhoneNumber("000-2222-2222");
-//        user.setGender("여성");
-//        userRepository.save(user);
-//
-//        user = new User();
-//        user.setEmail("user03");
-//        user.setPassword(passwordEncoder.encode("1234"));
-//        user.setRole(UserRole.USER);
-//        user.setName("바루스");
-//        user.setBirthday("2020-01-01");
-//        user.setPhoneNumber("000-3333-3333");
-//        user.setGender("남성");
-//        userRepository.save(user);
-//
-//        user = new User();
-//        user.setEmail("user@gmail.com");
-//        user.setPassword(passwordEncoder.encode("1234"));
-//        user.setRole(UserRole.USER);
-//        user.setName("이메일용");
-//        user.setBirthday("2020-02-02");
-//        user.setPhoneNumber("000-4444-4444");
-//        user.setGender("남성");
-//        userRepository.save(user);
-//    }
+    @PostConstruct
+    public void userInit() {
+        if (userRepository.findByEmail("admin") != null) {
+            return;
+        }
+
+        User user = new User();
+        user.setEmail("admin");
+        user.setPassword(passwordEncoder.encode("1234"));
+        user.setRole(UserRole.ADMIN);
+        user.setName("관리자");
+        user.setBirthday("0000-00-00");
+        user.setPhoneNumber("000-1234-5678");
+        user.setGender("성별");
+        userRepository.save(user);
+
+        user = new User();
+        user.setEmail("user01");
+        user.setPassword(passwordEncoder.encode("1234"));
+        user.setRole(UserRole.USER);
+        user.setName("홍길동");
+        user.setBirthday("2000-01-01");
+        user.setPhoneNumber("000-1111-1111");
+        user.setGender("남성");
+        userRepository.save(user);
+
+        user = new User();
+        user.setEmail("user02");
+        user.setPassword(passwordEncoder.encode("1234"));
+        user.setRole(UserRole.USER);
+        user.setName("블루이");
+        user.setBirthday("2010-01-01");
+        user.setPhoneNumber("000-2222-2222");
+        user.setGender("여성");
+        userRepository.save(user);
+
+        user = new User();
+        user.setEmail("user03");
+        user.setPassword(passwordEncoder.encode("1234"));
+        user.setRole(UserRole.USER);
+        user.setName("바루스");
+        user.setBirthday("2020-01-01");
+        user.setPhoneNumber("000-3333-3333");
+        user.setGender("남성");
+        userRepository.save(user);
+
+        user = new User();
+        user.setEmail("user@gmail.com");
+        user.setPassword(passwordEncoder.encode("1234"));
+        user.setRole(UserRole.USER);
+        user.setName("이메일용");
+        user.setBirthday("2020-02-02");
+        user.setPhoneNumber("000-4444-4444");
+        user.setGender("남성");
+        userRepository.save(user);
+    }
 
     @Transactional
     public void join(JoinDto joinDto) {
